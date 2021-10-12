f2beb460765cc847260a6a04b7a3ae76433c6b82
Arthur's Laptop <arthertz@iu.edu>
2021-10-12 13:38:20 -0400

    added input parameter for rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 
-export default {
-  input: 'js-build/modules/factorial.rkt.js',
-  plugins: [
-    getBabelOutputPlugin({
-      presets: ['@babel/preset-env']
-    })
-  ],
-  output: [
-    { file: 'bundle.js', format: 'cjs' },
-  ]
+export default commandLineArgs => {
+  return {
+    input: 'js-build/modules/' + commandLineArgs.input,
+    plugins: [
+      getBabelOutputPlugin({
+        presets: ['@babel/preset-env']
+      })
+    ],
+    output: [
+      { file: 'bundle.js', format: 'cjs' },
+    ]
+  }
 };
